6b628b20ad7b8b58c385b21c55f64ea296c03ceb
Koheinn <heinn2004@gmail.com>
2026-02-14 06:17:59 -0500

    Final Commit

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -10,15 +10,31 @@ def emotion_detector(text_to_analyze):
     }
 
     input_json = { "raw_document": { "text": text_to_analyze } }
+
     response = requests.post(url, headers=headers, json=input_json)
+
+    # Error handling for blank input
+    if response.status_code == 400:
+        return {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+        }
+
     response_dict = json.loads(response.text)
     emotions = response_dict['emotionPredictions'][0]['emotion']
+
     anger_score = emotions['anger']
     disgust_score = emotions['disgust']
     fear_score = emotions['fear']
     joy_score = emotions['joy']
     sadness_score = emotions['sadness']
+
     dominant_emotion = max(emotions, key=emotions.get)
+
     return {
         'anger': anger_score,
         'disgust': disgust_score,
